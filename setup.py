3c9d8b2e66188d0f7a86021721b92eef63ec92c7
Nicolás Frías <nicolas.friasrojas@gmail.com>
2023-03-26 03:29:02 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='vrplib_reader',
+    version="0.0.1",
+    url='https://github.com/nicosnow-cl/vrplib-reader',
+    author='Nicolás Frías',
+    author_email='nicolas.friasrojas@gmail.com',
+    packages=['src'],
+)
